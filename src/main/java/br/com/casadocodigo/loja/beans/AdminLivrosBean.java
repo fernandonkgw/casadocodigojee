eab7fe8a0fa036fa0932211e76227c1a8415e7c3
fernandonkgw <fernandonkgw@gmail.com>
2017-07-12 23:46:06 -0300

    criação do formulário e o bean

---
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -0,0 +1,21 @@
+package br.com.casadocodigo.loja.beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import br.com.casadocodigo.loja.models.Livro;
+
+@Named
+@RequestScoped
+public class AdminLivrosBean {
+
+	private Livro livro = new Livro();
+	
+	public void salvar() {
+		System.out.println("O livro foi salvo: " + livro);
+	}
+	
+	public Livro getLivro() {
+		return livro;
+	}
+}
